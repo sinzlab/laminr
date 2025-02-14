f531a62df1a24c61e50d40a561a45344ebee5d56
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2025-02-14 13:00:03 +0100

    Fix collections import

--- src/neuralpredictors/layers/cores/conv2d.py
+++ src/neuralpredictors/layers/cores/conv2d.py
@@ -1,9 +1,11 @@
 import logging
 import warnings
+from collections import OrderedDict
+
 try:
-    from collections.abc import Iterable, OrderedDict
+    from collections.abc import Iterable
 except ImportError:
-    from collections import Iterable, OrderedDict
+    from collections import Iterable
 
 from functools import partial
 
